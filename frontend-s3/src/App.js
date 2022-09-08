0591aace02fd68cabd0c4662c615d206d8e301d1
Mark Kort <485091@student.fontys.nl>
2022-09-08 14:09:31 +0200

    Added Links

--- frontend-s3/src/App.js
+++ frontend-s3/src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 import { useAuth0 } from "@auth0/auth0-react";
-import {Routes, Route, useNavigate, Navigate} from 'react-router-dom';
+import {Routes, Route, useNavigate, Navigate, BrowserRouter} from 'react-router-dom';
 import Profile from './Profile';
+import Navbar from './Navbar';
 function App() {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   return (
-    <><LoginButton /><LogoutButton /><Profile /></>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+        <Route path='/' element={<LoginButton />} />
+        <Route path='/profile' element={<Profile />} />
+    </Routes>
+    </BrowserRouter>
  );
 }
 
